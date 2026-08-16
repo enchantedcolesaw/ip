779d47b70d70d3886abb0ebf780c38c6ef3b95ca
enchantedcolesaw <yuxiao2004@gmail.com>
2026-08-17 00:31:15 +0800

    Changed chatbot name to Gatsby, added greeting, joke and farewell messages

--- AGENTS.md
+++ AGENTS.md
@@ -9,8 +9,8 @@ Unless the user says otherwise, assume that you are assisting a student working 
 # Student profile
 
 * Prior knowledge: Basic Java and OOP concepts.
-* Level of programming experience: [to be filled]
-* IDE and level of expertise: [to be filled]
+* Level of programming experience: Basic to Intermediate, typical year 2 computer science student without programming experience pre-college.
+* IDE and level of expertise: Basic familiarity with IDEs like IntelliJ, PyCharm, etc.
 
 # Guidance for interacting with users
 
---
+++ src/main/java/Gatsby.java
@@ -0,0 +1,14 @@
+public class Gatsby {
+    public static void main(String[] args) {
+        String banner = "************************************\n"
+                + "*              Gatsby              *\n"
+                + "************************************\n";
+        System.out.println(banner);
+        String welcome = "Wassup! I'm Gatsby.\n" + "What can I do for you?\n";
+        System.out.print(welcome);
+        String startJoke = "When did the Japanese invent eggs? A long tamago :)\n";
+        System.out.println(startJoke);
+        String goodbye = "Bye! Hope to see you again soon!\n";
+        System.out.println(goodbye);
+    }
+}
