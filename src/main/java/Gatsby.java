26b70c5eaef63c928df047c2df37c9afceaefe1c
enchantedcolesaw <yuxiao2004@gmail.com>
2026-08-18 22:33:23 +0800

    Add the ability to store whatever text the user enters and display it back when requested.
    Able to store up to 100 messages entered by the user.
    Chatbot now no longer just echoes what is entered by the user.
    Key in "list" to display the full list of texts entered by the user.

--- src/main/java/Gatsby.java
+++ src/main/java/Gatsby.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 import java.util.Set;
 
 /**
- * A simple command-line chatbot that echoes commands until the user enters the goodbye command.
+ * A simple command-line chatbot that stores and lists tasks until the user enters the goodbye command.
+ * Task list may be accessed when user enters the list command.
  */
 public class Gatsby {
     private static final String LINE = "____________________________________________________________";
     private static final Set<String> GOODBYE_COMMANDS = Set.of("bye", "byebye", "bye bye");
+    private static final String LIST_COMMAND = "list";
     private static final String GOODBYE_MESSAGE = " Bye. Hope to see you again soon!";
     private static final String START_JOKE = "When did the Japanese invent eggs? A long tamago :)";
-
+    private static final int MAX_TASKS = 100;
+    private static String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
     /**
      * Starts Gatsby and processes commands entered through standard input.
      *
@@ -36,11 +40,23 @@ public class Gatsby {
                 System.out.println(GOODBYE_MESSAGE);
                 System.out.println(LINE);
                 break;
+            } else if(isListCommand(command)) {
+                printList();
+            } else {
+                System.out.println(" added: " + command);
+                addToList(command);
+                System.out.println(LINE);
             }
-
-            System.out.println(" " + command);
-            System.out.println(LINE);
         }
+    }
+    /**
+     * Adds a user command into the list.
+     *
+     * @param command the raw command entered by the user
+     */
+    private static void addToList(String command){
+        tasks[taskCount] = command;
+        taskCount++;
     }
 
     /**
@@ -55,5 +71,28 @@ public class Gatsby {
                 .toLowerCase(Locale.ROOT)
                 .replaceAll("\\s+", " ");
         return GOODBYE_COMMANDS.contains(normalizedCommand);
+    }
+    /**
+     * Checks whether a command is one of Gatsby's accepted list commands.
+     * Repeated whitespace and differences in letter casing are ignored.
+     *
+     * @param command the raw command entered by the user
+     * @return true when the command should print out the internal list
+     */
+    private static boolean isListCommand(String command) {
+        String normalizedCommand = command.strip()
+                .toLowerCase(Locale.ROOT)
+                .replaceAll("\\s+", " ");
+        return LIST_COMMAND.equals(normalizedCommand);
+    }
+
+    /**
+     * Prints the latest snapshot of the task list at the time of this method call.
+     */
+    private static void printList() {
+        for (int i = 0; i < taskCount; i++){
+            System.out.println(" " + (i+1) + ". " +  tasks[i]);
+        }
+        System.out.println(LINE);
     }
 }
